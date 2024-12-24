cf9f6021edb1f1d31d3d1b7fdf6e05bc9409026e
dib fatima zohra <dibfatimazohra501@gmail.com>
2024-12-24 15:53:48 +0100

    php router

--- websites/dynamic_web/index.php
+++ websites/dynamic_web/index.php
@@ -1,5 +1,30 @@
-<?php
+<?php 
 
-$heading  = 'Home';
-require "functions.php";
-require "views/index.view.php";
+require 'functions.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes=[
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+
+];
+
+
+function routeToController($uri,$routes){
+    if(array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    }else{
+        abort();
+    }
+}
+function abort($code=404){
+
+    http_response_code( $code);
+    require "views/{$code}.php";
+    die();
+}
+
+
+routeToController($uri,$routes);
